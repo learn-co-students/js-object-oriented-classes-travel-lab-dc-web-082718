a01b4d20c09b7e4bbf2059966b066f7aa8a989f1
patti-c <patricia.carter@flatironschool.com>
2018-10-13 12:01:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 
+let eastWest =  ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    return(year - this.startDate.getFullYear());
+  }
+
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  avenueToInt(avenue) {
+    return eastWest.indexOf(avenue);
+  }
+
+  blocksTravelled() {
+    let horizontalStart = this.avenueToInt(this.beginningLocation.horizontal);
+    let verticalStart = (this.beginningLocation.vertical);
+    let horizontalEnd = this.avenueToInt(this.endingLocation.horizontal);
+    let verticalEnd = (this.endingLocation.vertical);
+
+    let horizontalDistance = Math.abs(horizontalEnd - horizontalStart);
+    let verticalDistance = Math.abs(verticalEnd - verticalStart);
+
+    return(horizontalDistance + verticalDistance);
+
+  }
+
+  estimatedTime(peak = false) {
+    if(peak === true) {
+      return (this.blocksTravelled() / 2);
+    } else {
+      return (this.blocksTravelled() / 3);
+    }
+  }
+
+}
